d30d0f13f486c3b970b3ea6b499d5f1168bc8987
Dmitry Kurson <kurson.d@gmail.com>
2022-11-07 19:10:29 +0200

    Added basic architecture

---
+++ Cryptocurency viewer/Data/Market.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptocurency_viewer.Data
+{
+    internal class Market
+    {
+    }
+}
