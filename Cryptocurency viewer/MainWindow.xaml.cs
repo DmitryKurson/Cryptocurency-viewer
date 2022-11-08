71db58c6abb1419d19a1dadf86e5072d57ba08ca
Dmitry Kurson <kurson.d@gmail.com>
2022-11-08 12:15:21 +0200

    Developed main menu design and basic logic

--- Cryptocurency viewer/MainWindow.xaml.cs
+++ Cryptocurency viewer/MainWindow.xaml.cs
@@ -23,6 +23,38 @@ namespace Cryptocurency_viewer
         public MainWindow()
         {
             InitializeComponent();
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+            //AssetsWindow win = new AssetsWindow();
+            //MainWindow win = new MainWindow();
+            //win.WindowState = System.Windows.WindowState.Maximized;
+            //win.WindowStyle = System.Windows.WindowStyle.None;
+            //win.Show();
+
+        }
+
+        private void assets_btn_Click(object sender, RoutedEventArgs e)
+        {
+            AssetsWindow assetswindow_obj = new AssetsWindow();
+            assetswindow_obj.ShowDialog();
+        }
+
+        private void exchanges_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ExchangesWindow exchangeswindow_obj = new ExchangesWindow();
+            exchangeswindow_obj.ShowDialog();
+        }
+
+        private void markets_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MarketWindow marketwindow_obj = new MarketWindow();
+            marketwindow_obj.ShowDialog();
+        }
+
+        private void exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            //AssetsWindow assetswindow_obj = new AssetsWindow();
+            //assetswindow_obj.ShowDialog();
         }
     }
 }
